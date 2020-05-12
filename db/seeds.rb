8e03c24beefe757e3622ab67c68250b77be86aad
Nicole Wong <nrewong@gmail.com>
2020-05-12 14:28:51 +0900

    add bootstrap and simple form

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  title = "#{Faker::Hacker.adjective} #{Faker::Hacker.noun}".titleize
+  content = "#{Faker::Hacker.noun.capitalize} #{Faker::Hacker.ingverb} -- #{Faker::Hacker.say_something_smart} #{Faker::Hacker.say_something_smart}"
+  article = Article.new(title: title, content: content)
+  article.save!
+end
